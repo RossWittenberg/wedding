2114dff213333c7925f29724fc56513310e9d355
mondolabs <development@mondo.com>
2016-11-08 14:38:09 -0500

    splash page fix

--- app/models/party.rb
+++ app/models/party.rb
@@ -69,3 +69,13 @@ class Party < ActiveRecord::Base
 end
 
 
+p = Party.find_by_party_name "Eli Maiman"
+g = Guest.create ({
+	first_name: "Alex",
+	last_name: "Talks",
+	party_name: "Eli Maiman",
+	party_id: p.id
+})
+
+
+
